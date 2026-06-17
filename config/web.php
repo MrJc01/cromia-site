082c04019ebef78b754bc5520e05c66613dd59b6
MrJ <78050688+MrJc01@users.noreply.github.com>
2026-06-17 03:50:02 -0300

    refactor: add class existence checks for debug and gii modules in environment configs

--- config/web.php
+++ config/web.php
@@ -65,20 +65,24 @@ $config = [
 ];
 
 if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => \yii\debug\Module::class,
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+    if (class_exists(\yii\debug\Module::class)) {
+        // configuration adjustments for 'dev' environment
+        $config['bootstrap'][] = 'debug';
+        $config['modules']['debug'] = [
+            'class' => \yii\debug\Module::class,
+            // uncomment the following to add your IP if you are not connecting from localhost.
+            //'allowedIPs' => ['127.0.0.1', '::1'],
+        ];
+    }
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => \yii\gii\Module::class,
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+    if (class_exists(\yii\gii\Module::class)) {
+        $config['bootstrap'][] = 'gii';
+        $config['modules']['gii'] = [
+            'class' => \yii\gii\Module::class,
+            // uncomment the following to add your IP if you are not connecting from localhost.
+            //'allowedIPs' => ['127.0.0.1', '::1'],
+        ];
+    }
 }
 
 return $config;
